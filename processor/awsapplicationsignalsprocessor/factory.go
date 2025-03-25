16c5eee2c36398fdbb6f1a92000f21ccc9f94792
harryryuu <harryryu@amazon.com>
2025-03-25 14:57:04 -0700

    Test

--- processor/awsapplicationsignalsprocessor/factory.go
+++ processor/awsapplicationsignalsprocessor/factory.go
@@ -21,7 +21,7 @@ const (
 
 var (
 	// The value of "type" key in configuration.
-	typeStr, _           = component.NewType("awsapplicationsignals")
+	typeStr, _           = component.NewType("awsapplicationsignalsprocessor")
 	consumerCapabilities = consumer.Capabilities{MutatesData: true}
 )
 
